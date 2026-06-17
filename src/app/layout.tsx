aca2699e364fc6cfe0e165f6644895e88c648d68
Rizal Firmansyah <rizalfirmansyah120593@gmail.com>
2026-06-17 13:14:49 +0700

    Integrate Adsterra popunder and banner ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Providers } from "@/components/Providers";
+import Script from 'next/script';
 
 const bebasNeue = Bebas_Neue({
   variable: "--font-bebas-neue",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <body
           className={`${bebasNeue.variable} ${inter.variable} antialiased bg-gray-800 text-white font-sans`}
         >
+          <Script src="https://pl29772446.effectivecpmnetwork.com/02/7e/f2/027ef20aa3ab234238482f60052e0581.js" />
           <Providers>
             {children}
           </Providers>
