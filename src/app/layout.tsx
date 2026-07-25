983c55cd32cc6a0932b7578a838c8217920c7f36
Rizal Firmansyah <rizalfirmansyah120593@gmail.com>
2026-07-25 08:32:30 +0700

    Update banner dan informasi sedekah

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Providers } from "@/components/Providers";
 import Script from 'next/script';
+import Script from "next/script";
 
 const bebasNeue = Bebas_Neue({
   variable: "--font-bebas-neue",
@@ -34,7 +35,16 @@ export default function RootLayout({
         <body
           className={`${bebasNeue.variable} ${inter.variable} antialiased bg-gray-800 text-white font-sans`}
         >
-          <Script src="https://pl29772446.effectivecpmnetwork.com/02/7e/f2/027ef20aa3ab234238482f60052e0581.js" />
+          {/* Iklan 1: Social Bar */}
+          <script src="https://alwaysmulticulturallanding.com/a2/e0/3c/a2e03cf1bed0e9d731c3812e05e4517c.js"></script>
+
+          {/* Iklan 1: Popunder */}
+          <script src="https://alwaysmulticulturallanding.com/fe/8f/81/fe8f815f75fcefd6fa17243386912ae0.js"></script>
+
+          {/* Iklan 1: Native Banner */}
+          <script async="async" data-cfasync="false" src="https://alwaysmulticulturallanding.com/bcf5164b464a8e3c0b486cd50f77b999/invoke.js"></script>
+          <div id="container-bcf5164b464a8e3c0b486cd50f77b999"></div>
+
           <Providers>
             {children}
           </Providers>
